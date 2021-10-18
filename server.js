03d6eceecf61245fa3054f571d0f5a98afb00130
akash <javascript.mspl@gmail.com>
2021-10-18 16:07:15 +0530

    initial commit comment

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser')
+// Require Notes routes
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+require('./app/routes/note.routes')(app);
+
+const host ='localhost'
+const port ='3000'
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+
+app.get('/', (req, res) => {
+  res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    // res.sendFile(__dirname + '/index.html')
+  })
+
+  // app.post('/quotes', (req, res) => {
+  //   console.log('response!')
+  //   console.log(req.body)
+  // })
+
+
+app.listen(port,host, function() {
+    console.log('listening on','http://'+host+':'+port)
+  })
